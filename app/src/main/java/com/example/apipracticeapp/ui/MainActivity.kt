d5e94789e3ba142f91cb500ac4018b7d547527b7
たかっしー <takashiho0913@gmail.com>
2022-08-29 01:06:47 +0900

    [add] MainActivityをHilt対応に

--- app/src/main/java/com/example/apipracticeapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/apipracticeapp/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.apipracticeapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.apipracticeapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
