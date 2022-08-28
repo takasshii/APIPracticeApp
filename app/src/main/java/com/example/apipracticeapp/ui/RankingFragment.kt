d98ed019fb8713bb1c2883df3c27a80dedc34179
たかっしー <takashiho0913@gmail.com>
2022-08-29 01:05:44 +0900

    [add] 決定ボタンを押したときに関数が走るようにした

--- app/src/main/java/com/example/apipracticeapp/ui/RankingFragment.kt
+++ app/src/main/java/com/example/apipracticeapp/ui/RankingFragment.kt
@@ -1,18 +1,24 @@
 package com.example.apipracticeapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.apipracticeapp.R
 import com.example.apipracticeapp.databinding.FragmentSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class SearchFragment : Fragment() {
+@AndroidEntryPoint
+class RankingFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: RankingViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,14 +26,18 @@ class SearchFragment : Fragment() {
     ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
 
-        binding.searchEditText.setOnEditorActionListener { textView, actionId, keyEvent ->
+        binding.searchEditText.setOnEditorActionListener { editText, actionId, keyEvent ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_NULL) {
+                editText.text.toString().let {
+                    // 入力された文字を代入
+                    viewModel.fetchAPI(it)
+                    Log.v("result", it)
+                }
                 navigationResultFragment()
                 return@setOnEditorActionListener true
             }
             return@setOnEditorActionListener false
         }
-
         return binding.root
     }
 
