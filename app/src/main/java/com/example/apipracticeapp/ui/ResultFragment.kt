49dd558994e02f4146fec002c4f86151113324d6
たかっしー <takashiho0913@gmail.com>
2022-08-29 01:06:01 +0900

    [add] ResultFragmentをHilt対応に

--- app/src/main/java/com/example/apipracticeapp/ui/ResultFragment.kt
+++ app/src/main/java/com/example/apipracticeapp/ui/ResultFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.apipracticeapp.databinding.FragmentResultBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ResultFragment : Fragment() {
     private var _binding: FragmentResultBinding? = null
     private val binding get() = _binding!!
