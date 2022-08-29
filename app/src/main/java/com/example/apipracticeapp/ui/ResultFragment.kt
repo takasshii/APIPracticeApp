866a6d05f1f522ffeb39e25e50b5308d56f6a4fe
たかっしー <takashiho0913@gmail.com>
2022-08-29 20:28:49 +0900

    [add] 値をセットして表示

--- app/src/main/java/com/example/apipracticeapp/ui/ResultFragment.kt
+++ app/src/main/java/com/example/apipracticeapp/ui/ResultFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import coil.load
 import com.example.apipracticeapp.databinding.FragmentResultBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,11 +14,26 @@ import dagger.hilt.android.AndroidEntryPoint
 class ResultFragment : Fragment() {
     private var _binding: FragmentResultBinding? = null
     private val binding get() = _binding!!
+
+    private val args: ResultFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentResultBinding.inflate(inflater, container, false)
+
+        // 値を受け取る
+        val item = args.item
+
+        binding.imageView.load(item.ownerIconUrl)
+        binding.repositoryNameText.text = item.name
+        binding.languageRightText.text = item.language
+        binding.starsRightText.text = "${item.stargazersCount} stars"
+        binding.watchersRightText.text = "${item.watchersCount} watchers"
+        binding.forkRightText.text = "${item.forksCount} forks"
+        binding.issueRightText.text = "${item.openIssuesCount} open issues"
+
 
         return binding.root
     }
