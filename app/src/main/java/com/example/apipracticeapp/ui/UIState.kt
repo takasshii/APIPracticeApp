79302677ff2bfb0952714b6c865ec9bd590ba9ab
たかっしー <takashiho0913@gmail.com>
2022-08-30 21:01:52 +0900

    [fix] timeの状態変数を追加

--- app/src/main/java/com/example/apipracticeapp/ui/UIState.kt
+++ app/src/main/java/com/example/apipracticeapp/ui/UIState.kt
@@ -7,11 +7,12 @@ import java.util.*
 data class UiState(
     val repositories: List<Item>?,
     val events: List<Event> = emptyList(),
+    val time: String?,
     val proceeding: Boolean
 )
 
 sealed interface Event {
-    data class Success(val time: String) : Event
+    object Success : Event
     data class Error(val message: String) : Event
     data class NextPage(val item: Item): Event
 }
