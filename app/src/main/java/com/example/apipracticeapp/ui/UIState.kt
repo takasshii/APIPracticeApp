f7593dd6d5d4e112c230ad15529f5fe40c53cb70
たかっしー <takashiho0913@gmail.com>
2022-09-02 13:46:32 +0900

    [add] Pagerで必要のない部分を削除

--- app/src/main/java/com/example/apipracticeapp/ui/UIState.kt
+++ app/src/main/java/com/example/apipracticeapp/ui/UIState.kt
@@ -1,18 +1,14 @@
 package com.example.apipracticeapp.ui
 
 import com.example.apipracticeapp.data.Item
-import com.example.apipracticeapp.data.JsonGithub
-import java.util.*
 
 data class UiState(
-    val repositories: List<Item>?,
     val events: List<Event> = emptyList(),
     val time: String?,
-    val proceeding: Boolean
 )
 
 sealed interface Event {
     object Success : Event
     data class Error(val message: String) : Event
-    data class NextPage(val item: Item): Event
+    data class NextPage(val item: Item) : Event
 }
