8f709bac4ead5876799d108bba34b789010ee6b4
たかっしー <takashiho0913@gmail.com>
2022-09-02 13:43:33 +0900

    [add]　Pagerを返すように変更、ページをたくさん返す様に変更

--- app/src/main/java/com/example/apipracticeapp/data/GithubAPIRepositoryImpl.kt
+++ app/src/main/java/com/example/apipracticeapp/data/GithubAPIRepositoryImpl.kt
@@ -1,12 +1,13 @@
 package com.example.apipracticeapp.data
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
-import java.lang.Exception
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -14,32 +15,26 @@ import javax.inject.Singleton
 class GithubAPIRepositoryImpl @Inject constructor(
     private val apiService: ApiService
 ) : GithubAPIRepository {
-    override suspend fun getRepository(
-        header: String,
-        inputText: String
-    ): APIResult<JsonGithub?> =
-        withContext(Dispatchers.IO) {
-            try {
-                // 成功時
-                val apiResult =
-                    apiService.fetchRepositoryData(header, inputText)
-                // WeatherAPIResultに変形
-                APIResult.Success(
-                    data = apiResult.body()
-                )
-            } catch (e: Exception) {
-                // 失敗時
-                APIResult.Error(
-                    exception = e
-                )
-            }
-        }
+    override fun getRepository(): Flow<PagingData<Item>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = NETWORK_PAGE_SIZE,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { GithubPagingSource(apiService) }
+        ).flow
+    }
+
+    companion object {
+        const val NETWORK_PAGE_SIZE = 30
+    }
 }
 
 interface ApiService {
-    @GET("search/repositories")
+    @GET("search/repositories?q=stars:>1")
     suspend fun fetchRepositoryData(
         @Header("Accept") header: String,
-        @Query("q") inputText: String
+        @Query("page") page: Int,
+        @Query("per_page") itemsPerPage: Int
     ): Response<JsonGithub>
 }
